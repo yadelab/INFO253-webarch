fb6a0a7d455f41c0a47d6e97ec02af87b9389046
Kay Ashaolu <kashaolu@ancestry.com>
2015-09-25 12:25:07 -0700

    Adding templating

--- hw-5/app.py
+++ hw-5/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, redirect, make_response
+from flask import Flask, request, redirect, make_response, render_template
 from subprocess import check_output
 app = Flask(__name__, static_folder="public_html/static")
 import pprint
+import shelve
 
-# Demo in class: dictionary that holds variables we store
-demo_dict = dict()
+# Creating a database for our server. This way even if our server shuts down 
+# we dcan save information
+demo_db = shelve.open("demo_db.db")
 
 # Demo in class: POST
 @app.route('/demo/wiki', methods=["POST", "PUT"])
@@ -12,56 +14,30 @@ def load_demo_post():
 
 	post_id = request.form.get("id")
 	post_title = request.form.get("title")
-	demo_dict[post_id] = post_title
+	demo_db[post_id] = post_title
 	
-	content = '''
-	<html>
-		<head>
-			<title>Demo for class today</title>
-		</head>
-		<body>
-			<h1>Stored wiki entry: ''' + post_id + ''' => ''' + post_title + '''</h1>
-		</body>
-	</html>'''
-	return content, 200
+	return render_template("wiki_post.html", id=post_id, title=post_title)
 
 # Demo in class: GET
 @app.route('/demo/wiki', methods=["GET"])
 def load_demo_get():
-		
-	content = '''
-	<html>
-		<head>
-			<title>Demo for class today</title>
-		</head>
-		<body>
-			<pre>''' + pprint.pformat(demo_dict) + '''</pre>
-		</body>
-	</html>'''
-	return content, 200
+
+	all_data = pprint.pformat(demo_db)
+	return render_template("wiki_get.html", data=all_data)
 
 ###############################################################
 
 # HW 5 content
 
-# Variable that holds current redirect path
-redirect_path = "http://ischool.berkeley.edu"
+# Creating database to store redirect
+redirect_db = shelve.open("redirect.db")
 
 # When you type http://127.0.0.1:5000/home in your browser, this
 # piece of code is run
 @app.route('/home', methods=["GET"])
 def load_home():
-	content = '''
-<html>
-	<head>
-		<title>INFO 253: Web Architecture</title>
-	</head>
-	<body>
-		<h1>''' + request.args.get('name') + '''</h1>
-	</body>
-</html>'''
-
-	return content, 200
+	name = request.args.get('name', 'This should say your name')
+	return render_template("home.html", name=name)
 
 # When you type http://127.0.0.1:5000/school in your browser, this
 # piece of code is run
@@ -69,30 +45,19 @@ def load_home():
 def load_redirect():
 	global redirect_path
 
-
 	if request.method == "GET":
-		return redirect(redirect_path, code=302)
+		return redirect(redirect_db["location"], code=302)
 	else: # If the method is either POST or PUT
-		
 		# Change the redirect path to the parameter named "name"
-		redirect_path = request.form.get('location').strip()
+		redirect_db["location"] = request.form.get('location', "http://ischool.berkeley.edu").strip() # Remove newline at end of telnet requests
 		
-		content = '''
-	<html>
-		<head>
-			<title>Redirect path changed</title>
-		</head>
-		<body>
-			<h1>/redirect now points to: ''' + request.form.get('location') + '''</h1>
-		</body>
-	</html>'''
-	return content, 200
+		return render_template("redirect.html", location=redirect_db["location"])
 
 # When you type http://127.0.0.1:5000/image in your browser, this
 # piece of code is run
 @app.route('/image')
 def image():
-	"""Returns a PNG image"""
+	"""Returns an image"""
 
 	in_file = open("public_html/static/logo_home", "rb")
 	data = in_file.read()
